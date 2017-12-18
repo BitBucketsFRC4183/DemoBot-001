42f20b32156bbd6385621a3451e76678bd6bdd9d
sabzer <zerefasaba@gmail.com>
2017-12-17 17:13:11 -0700

    DriveSubsystem, RobotMap, PS4Constants

--- src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsystem.java
+++ src/org/usfirst/frc/team4183/robot/subsystems/DriveSubsystem.java
@@ -2,17 +2,44 @@ package org.usfirst.frc.team4183.robot.subsystems;
 
 import edu.wpi.first.wpilibj.command.Subsystem;
 
-/**
- *
- */
-public class DriveSubsystem extends Subsystem {
+import com.ctre.*;
+import edu.wpi.first.wpilibj.RobotDrive;
+import org.usfirst.frc.team4183.robot.RobotMap;
 
-    // Put methods for controlling this subsystem
-    // here. Call these from Commands.
+public class DriveSubsystem extends Subsystem 
+{
+	CANTalon leftMotorA;
+	CANTalon leftMotorB;
+	
+	CANTalon rightMotorA;
+	CANTalon rightMotorB;
+	
+	RobotDrive driveRobot;
+	
+	public static void main(String[] args)
+	{
+		
+	}
+	
+	public void initDefaultCommand()
+	{
+		//set the default command for a subsystem here.
+		//setDefaultCommand(new MySpecialCommand());
+	}
+	public DriveSubsystem()
+	{
+		rightMotorA = new CANTalon(RobotMap.RIGHT_FRONT_MOTOR_ID);
+		rightMotorB = new CANTalon(RobotMap.RIGHT_REAR_MOTOR_ID);
+		
+		leftMotorA = new CANTalon(RobotMap.LEFT_FRONT_MOTOR_ID);
+		leftMotorB = new CANTalon(RobotMap.LEFT_REAR_MOTOR_ID);
+		
+		driveRobot = new RobotDrive(leftMotorA, leftMotorB, rightMotorA, rightMotorB);
+	}
+	
+	public void arcadeDrive(double forwardValue, double turnValue)
+	{
+		driveRobot.arcadeDrive(forwardValue, turnValue);
+	}
 
-    public void initDefaultCommand() {
-        // Set the default command for a subsystem here.
-        //setDefaultCommand(new MySpecialCommand());
-    }
 }
-
